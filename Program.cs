af2d06cb517955004e04f4c6e7d5c80f3eed702e
jhonatacamargol <jhonatapires32@hotmail.com>
2023-01-27 16:05:08 -0300

    atualização tarefa

--- Program.cs
+++ Program.cs
@@ -25,6 +25,8 @@ namespace SistemaDeTarefas
 
             builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 
+            builder.Services.AddScoped<ITarefaRepositorio, TarefaRepositorio>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
